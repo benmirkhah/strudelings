2b6061ff78677799c62f307063ee47466aaa76db
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2025-11-13 20:20:39 -0700

    Adds kicks

---
+++ strudel-kicks.js
@@ -0,0 +1,299 @@
+let kicks = Array();
+
+kicks = [  //Favorites are stared *
+  "9000_bd",                  //0 *
+  "bd:1",                     //1
+  "bd:2",                     //2
+  "bd:3",                     //3 *
+  "bd:4",                     //4
+  "bd:5",                     //5
+  "bd:6",                     //6 *
+  "bd:7",                     //7 *
+  "ace_bd:1",                 //8 *
+  "ace_bd:2",                 //9
+  //------------------------------
+  "ace_bd:3",                 //10
+  "ajkpercusyn_bd",           //11 *
+  "akailinn_bd",              //12 *
+  "akaimpc60_bd:2",           //13
+  "akaixr10_bd:1",            //14
+  "akaixr10_bd:2",            //15 *
+  "akaixr10_bd:6",            //16
+  "akaixr10_bd:7",            //17 *
+  "akaixr10_bd:9",            //18 *
+  "alesishr16_bd",            //19 *
+  //------------------------------
+  "bossdr110_bd",             //20 *
+  "bossdr55_bd:1",            //21 *
+  "bossdr55_bd:2",            //22 *
+  "bossdr550_bd:1",           //23 
+  "bossdr550_bd:2",           //24
+  "bossdr550_bd:4",           //25
+  "casiorz1_bd",              //26 *
+  "circuitstom_bd",           //27 *
+  "circuitsdrumtracks_bd",    //28
+  "compurhythm78_bd",         //29 *
+  //------------------------------
+  "compurhythm1000_bd",       //30
+  "compurhythm8000_bd",       //31
+  "concertmatemg1_bd:1",      //32 *
+  "concertmatemg1_bd:2",      //33 *
+  "concertmatemg1_bd:3",      //34
+  "d110_bd",                  //35
+  "d70_bd:3",                 //36
+  "ddm110_bd",                //37
+  "ddr30_bd:6",               //38
+  "ddr30_bd:7",               //39
+  //------------------------------
+  "ddr30_bd:8",               //40
+  "dmx_bd:1",                 //41 *
+  "dmx_bd:2",                 //42 *
+  "dmx_bd:3",                 //43 
+  "doepferms404_bd:1",        //44 *
+  "doepferms404_bd:2",        //45 *
+  "dpm48_bd:3",               //46
+  "dr110_bd",                 //47
+  "dr55_bd:1",                //48
+  "dr55_bd:2",                //49
+  //------------------------------
+  "dr550_bd:1",               //50
+  "dr550_bd:2",               //51
+  "drumulator_bd",            //52 *
+  "emudrumulator_bd",         //53 *
+  "emumodular_bd:1",          //54 * 
+  "emusp12_bd:4",             //55
+  "emusp12_bd:6",             //56
+  "emusp12_bd:7",             //57
+  "emusp12_bd:8",             //58
+  "emusp12_bd:9",             //59
+  //------------------------------
+  "emusp12_bd:11",            //60
+  "emusp12_bd:12",            //61
+  "emusp12_bd:13",            //62
+  "hr16_bd",                  //63
+  "jd990_bd:1",               //64
+  "jd990_bd:3",               //65
+  "jd990_bd:4",               //66
+  "jd990_bd:5",               //67
+  "jd990_bd:6",               //68
+  "jd990_bd:8",               //69
+  //------------------------------
+  "jd990_bd:10",              //70
+  "korgddm110_bd",            //71 *
+  "korgkpr77_bd",             //72 *
+  "korgkr55_bd",              //73 *
+  "korgm1_bd:2",              //74
+  "korgm1_bd:3",              //75 * 
+  "korgminipops_bd:1",        //76
+  "korgminipops_bd:5",        //77 *
+  "korgpoly800_bd:1",         //78 *
+  "korgpoly800_bd:2",         //79 * <<DUP
+  //-----------------------------
+  "korgpoly800_bd:2",         //80 *
+  "korgpoly800_bd:3",         //81 *
+  "korgpoly800_bd:4",         //82 *
+  "korgt3_bd:1",              //83 *
+  "korgt3_bd:2",              //84
+  "korgt3_bd:3",              //85
+  "kpr77_bd",                 //86 *
+  "kr55_bd",                  //87 *
+  "linn_bd",                  //88
+  "linn9000_bd",              //89
+  //------------------------------
+  "linndrum_bd",              //90
+  "linnlm1_bd:1",             //91
+  "linnlm1_bd:2",             //92
+  "linnlm1_bd:3",             //93 *
+  "linnlm1_bd:4",             //94
+  "linnlm2_bd:1",             //95
+  "lm8953_bd:3",              //96
+  "m1_bd:2",                  //97
+  "m1_bd:3",                  //98 *
+  "mc202_bd:1",               //99
+  //------------------------------
+  "mc202_bd:5",               //100 *
+  "mc303_bd:1",               //101
+  "mc303_bd:2",               //102
+  "mc303_bd:10",              //103
+  "mc303_bd:11",              //104
+  "mc303_bd:12",              //105 *
+  "mc303_bd:16",              //106 *
+  "mfb512_bd",                //107
+  "minipops_bd:1",            //108
+  "minipops_bd:2",            //109
+  //-------------------------------
+  "minipops_bd:4",            //110
+  "minipops_bd:5",            //111 *
+  "minipops_bd:6",            //112
+  "minipops_bd:7",            //113
+  "moogconcertmatemg1_bd:1",  //114 *
+  "moogconcertmatemg1_bd:2",  //115 *
+  "moogconcertmatemg1_bd:3",  //116 *
+  "mpc1000_bd:1",             //117
+  "mpc1000_bd:3",             //118 *
+  "mpc1000_bd:5",             //119
+  //-------------------------------
+  "mpc60_bd:2",               //120
+  "ms404_bd:1",               //121 *
+  "ms404_bd:2",               //122 *
+  "mt32_bd",                  //123 *
+  "oberheimdmx_bd:1",         //124
+  "oberheimdmx_bd:2",         //125
+  "oberheimdmx_bd:3",         //126
+  "percysyn_bd",              //127
+  "r88_bd",                   //128 *
+  "rhodespolaris_bd:1",       //129
+  //-------------------------------
+  "rhodespolaris_bd:2",       //130 *
+  "rhodespolaris_bd:3",       //131 *
+  "rhodespolaris_bd:4",       //132
+  "rhythmace_bd:1",           //133
+  "rhythmace_bd:2",           //134
+  "rhythmace_bd:3",           //135
+  "rolandcompurhythm1000_bd", //136
+  "rolandcompurhythm78_bd",   //137 *
+  "rolandcompurhythm8000_bd", //138
+  "rolandd110_bd",            //139 *
+  //------------------------------
+  "rolandd70_bd:2",           //140
+  "rolandd70_bd:3",           //141 *
+  "rolandddr30_bd:2",         //142
+  "rolandddr30_bd:3",         //143
+  "rolandddr30_bd:4",         //144
+  "rolandddr30_bd:6",         //145
+  "rolandddr30_bd:7",         //146
+  "rolandddr30_bd:8",         //147
+  "rolandjd990_bd:1",         //148
+  "rolandjd990_bd:3",         //149
+  //-------------------------------
+  "rolandjd990_bd:4",         //150 *
+  "rolandjd990_bd:5",         //151 *
+  "rolandjd990_bd:6",         //152
+  "rolandjd990_bd:8",         //153
+  "rolandjd990_bd:10",        //154
+  "rolandmc202_bd:1",         //155
+  "rolandmc202_bd:5",         //156
+  "rolandmc303_bd:1",         //157
+  "rolandmc303_bd:2",         //158 *
+  "rolandmc303_bd:3",         //159 *
+  //-------------------------------
+  "rolandmc303_bd:8",         //160 *
+  "rolandmc303_bd:10",        //161 *
+  "rolandmc303_bd:11",        //162
+  "rolandmc303_bd:12",        //163
+  "rolandmc303_bd:13",        //164 *
+  "rolandmc303_bd:16",        //165
+  "rolandmt32_bd",            //166
+  "rolandr8_bd:1",            //167
+  "rolandr8_bd:2",            //168
+  "rolandr8_bd:3",            //169
+  //-------------------------------
+  "rolandr8_bd:4",            //170 *
+  "rolandr8_bd:6",            //171
+  "rolandr8_bd:7",            //172 *
+  "rolands50_bd:1",           //173
+  "rolands50_bd:4",           //174 *
+  "rolandsystem100_bd:1",     //175 *
+  "rolandsystem100_bd:3",     //176 *
+  "rolandsystem100_bd:4",     //177
+  "rolandsystem100_bd:7",     //178
+  "rolandsystem100_bd:10",    //179 *
+  //-------------------------------
+  "rolandsystem100_bd:15",    //180 *
+  "rolandtr505_bd",           //181 *
+  "rolandtr606_bd",           //182
+  "rolandtr626_bd:1",         //183
+  "rolandtr626_bd:2",         //184 *
+  "rolandtr707_bd:1",         //185 *
+  "rolandtr707_bd:2",         //185 *
+  "rolandtr909_bd:1",         //186
+  "rolandtr909_bd:2",         //187 *
+  "rolandtr909_bd:3",         //188 *
+  "rolandtr909_bd:4",         //189 *
+  //-------------------------------
+  "rx21_bd",                  //190
+  "rx5_bd:1",                 //191 *
+  "rx5_bd:2",                 //192
+  "ry30_bd:1",                //193
+  "ry30_bd:9",                //194 *
+  "ry30_bd:12",               //195 *
+  "ry30_bd:13",               //196 *
+  "rz1_bd",                   //197 *
+  "s50_bd:1",                 //198 *
+  "s50_bd:4",                 //199
+  //-------------------------------
+  "sakatadpm48_bd:1",         //200
+  "sakatadpm48_bd:2",         //201
+  "sakatadpm48_bd:3",         //202 *
+  "sds5_bd:8",                //203 *
+  "sds5_bd:12",               //204 *
+  "sequentialcircuitstom_bd", //205 *
+  "simmonssds5_bd:1",         //206
+  "simmonssds5_bd:2",         //207
+  "simmonssds5_bd:3",         //208
+  "simmonssds5_bd:4",         //209
+  //-------------------------------
+  "simmonssds5_bd:5",         //210
+  "simmonssds5_bd:6",         //211
+  "simmonssds5_bd:7",         //212
+  "simmonssds5_bd:8",         //213 *
+  "simmonssds5_bd:9",         //214
+  "simmonssds5_bd:10",        //215
+  "simmonssds5_bd:11",        //216
+  "simmonssds5_bd:12",        //217 *
+  "soundmastersr88_bd",       //218 *
+  "sequentialcircuitsdrumtracks_bd",
+  //-------------------------------
+  "sp12_bd:3",                //220 *
+  "sp12_bd:4",                //221 *
+  "sp12_bd:6",                //222
+  "sp12_bd:7",                //223 *
+  "sp12_bd:8",                //224 *
+  "sp12_bd:9",                //225 *
+  "sp12_bd:10",               //226
+  "sp12_bd:11",               //227
+  "sp12_bd:14",               //228 *
+  "spacedrum_bd:1",           //229
+  //-------------------------------
+  "spacedrum_bd:2",           //230 *
+  "spacedrum_bd:3",           //231
+  "spacedrum_bd:4",           //232
+  "spacedrum_bd:5",           //233
+  "spacedrum_bd:7",           //234
+  "spacedrum_bd:10",          //235 *
+  "spacedrum_bd:11",          //236 *
+  "sr16_bd:1",                //237 *
+  "sr16_bd:2",                //238
+  "sr16_bd:3",                //239
+  //-------------------------------
+  "sr16_bd:4",                //240 *
+  "sr16_bd:6",                //241
+  "sr16_bd:7",                //242
+  "sr16_bd:8",                //243
+  "sr16_bd:11",               //244
+  "sr16_bd:13",               //245
+  "t3_bd:1",                  //246 *
+  "t3_bd:2",                  //247 *
+  "t3_bd:3",                  //248
+  "t3_bd:4",                  //249
+  //-------------------------------
+  "t3_bd:5",                  //250
+  "tg33_bd:1",                //251
+  "tg33_bd:3",                //252
+  "tg33_bd:4",                //253
+  "tr808_bd:1",               //254
+  "tr808_bd:6",               //255
+  "tr808_bd:16",              //256
+  "tr808_bd:20",              //257
+  "xdrumlm8953_bd:3",         //258
+  "yamaharx21_bd",            //259
+  //-------------------------------
+  "xr10_bd:1",                //260
+  "xr10_bd:2",                //261 *
+  "xr10_bd:4",                //262 *
+  "xr10_bd:6",                //263
+  "xr10_bd:7",                //264
+  "xr10_bd:9",                //265
+  "yamaharx5_bd:1",           //266 *
+  "yamaharx5_bd:2",           //267
+]; 
