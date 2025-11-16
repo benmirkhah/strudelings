4f66bce9c9932f5103a94fb7d2015301c32ea701
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2025-11-16 10:01:56 -0700

    Wooden sounds added

---
+++ wood.js
@@ -0,0 +1,98 @@
+var knocks = [  //Knocks on wood
+  "rim:1",                              //Zero
+  "jazz:2",                             //Knock 1  *
+  "misc:1",                             //Knock 2  *
+  "rhythmace_perc:1",                   //Knock 3  *
+  "rolandsystem100_misc:2",             //Knock 4  *
+  "korgminipops_misc:3",                //Knock 5  *
+  "rolandr8_perc:2",                    //Knock 6  *
+  "yamaharm50_perc:7",                  //Knock 7  *
+  "yamaharm50_perc:14",                 //Knock 8  *
+  "rhythmace_perc:6",                   //Knock 9  *
+  "rhythmace_perc:5",                   //Knock 10 *
+  "rolandsystem100_perc:15",            //Knock 11 *
+  "rolandmt32_perc:3",                  //Knock 12 *
+  "rolandd110_perc:1",                  //Knock 13 *
+  "bossdr550_perc:5",                   //Knock 14 *
+  "rolandtr505_perc:3",                 //Knock 15
+  "rolands50_perc:6",                   //Knock 16
+];
+
+var sticks = [  //Drum Sticks
+  "sd:1",                               //Zero
+  "alesishr16_rim",                     //Sticks 1 *
+  "rolandd70_perc",                     //Sticks 2 *
+  "yamahary30_perc:6",                  //Sticks 3 *
+  "korgt3_perc:3",                      //Sticks 4 *
+];
+
+var wkicks = [  //Wooden Kicks
+  "bd:1",                               //Zero
+  "sergemodular_perc:5",                //WKick 1 *
+  "yamaharm50_perc:30",                 //WKick 2 *
+  "yamaharm50_perc:29",                 //WKick 3 *
+  "yamaharm50_perc:28",                 //WKick 4 *
+  "rolandsystem100_perc:10",            //WKick 5 *
+  "yamahary30_misc:1",                  //WKick 6
+  "mpc1000_bd:4",                       //WKick 7
+  "linnlm2_bd:2",                       //WKick 8
+  "linnlm2_bd:3",                       //WKick 9
+  "linnlm2_bd:4",                       //WKick 10
+  "yamahary30_misc:2",                  //WKick 11
+  "simmonssds5_rim:3",                  //WKick 12
+];
+
+var wsnares = [  //Wooden Snares
+  "sd:1",                               //Zero
+  "compurhythm1000_perc:3",             //WSD *
+  "rolandmt32_perc:2",                  //WSD *
+  "yamaharm50_perc:6",                  //WSD *
+  "yamaharm50_perc:8",                  //WSD
+  "yamaharm50_perc:9",                  //WSD
+  "yamaharm50_perc:13",                 //WSD
+  "yamahatg33_perc:3",                  //WSD
+  "akaimpc60_perc:1",                   //WSD
+  "yamahatg33_rim",                     //WSD
+  "akaimpc60_rim",                      //WSD
+];
+
+var tiks = [  //Wooden Tiks
+  "sd:1",                               //Zero
+  "rolandtr808_rim",                    //Tik 1  *
+  "korgm1_misc:6",                      //Tik 2  *
+  "korgm1_perc:3",                      //Tik 3  *
+  "linnlm1_perc:2",                     //Tik 4  *
+  "xdrumlm8953_rim:2",                  //Tik 5  *
+  "simmonssds5_rim:1",                  //Tik 6  *
+  "rolandmt32_perc:7",                  //Tik 7  *
+  "rolandr8_rim:2",                     //Tik 8  *
+  "korgm1_misc:5",                      //Tik 9  *
+  "simmonssds5_rim:2",                  //Tik 10 *
+  "simmonssds5_rim:4",                  //Tik 11 *
+  "rolands50_perc:12",                  //Tik 12
+  "linndrum_rim:1",                     //Tik 13
+  "rolandr8_rim:1",                     //Tik 14
+  "lm8953_rim:2",                       //Tik 15 
+  "korgt3_rim",                         //Tik 16
+  "korgm1_rim",                         //Tik 17
+  "sp12_bd:12",                         //Tik 18
+];
+
+var toks = [  //Wooden Toks
+  "sd:1",                               //Zero
+  "rolandtr909_rim:2",                  //Tok 1 *
+  "rolandtr909_rim:3",                  //Tok 2 *
+  "yamaharm50_perc:50",                 //Tok 3 *
+  "casiorz1_rim",                       //Tok 4 *
+  "rolandtr707_rim",                    //Tok 5 *
+  "rolandtr505_rim",                    //Tok 6 *
+  "akaixr10_perc:5",                    //Tok 7 *
+  "rolandtr626_perc:5",                 //Tok
+  "akaixr10_perc:3",                    //Tok
+  "emudrumulator_rim",                  //Tok
+  "korgddm110_rim",                     //Tok
+  "rolands50_perc:4",                   //Tok
+  "yamaharx5_rim",                      //Tok
+  "linnlm1_rim",                        //Tok
+  "rolandcompurhythm1000_rim",          //Tok
+];
