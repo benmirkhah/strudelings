abae34e380674b48dbe2599fb2f45e5b990bea33
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2025-11-15 18:18:26 -0700

    Typewriter kit added

---
+++ type.js
@@ -0,0 +1,15 @@
+var typewriter = [  //Sounds like old typewriters
+  "rolandmc303_perc:30",                //1  Bell
+  "yamahary30_misc:8",                  //2  Clap
+  "rolandmc303_perc:34",                //3  Hat
+  "rolandmc303_perc:28",                //4  Hat
+  "yamaharm50_misc:9",                  //5  Kick
+  "sergemodular_perc:1",                //6  Kick  
+  "rolandjd990_perc:3",                 //7  Ride
+  "yamaharm50_perc:55",                 //8  Rim
+  "simmonssds5_rim:7",                  //9  Rim
+  "rolandjd990_misc:8",                 //10 Rim
+  "yamahary30_rim:1",                   //11 Snare
+  "yamaharm50_perc:23",                 //12 Tom
+  "yamaharm50_perc:24",                 //13 Tom
+];
