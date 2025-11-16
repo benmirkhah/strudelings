39cdd790512501ffc1df73892f8177d49d061935
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2025-11-15 17:28:55 -0700

    Whistle sounds added

---
+++ whistle.js
@@ -0,0 +1,15 @@
+var whistles = [
+  "yamaharm50_perc:26",                 //Whistle Short *
+  "bossdr550_perc:10",                  //Whistle Short
+  "rolandmt32_perc:11",                 //Whistle Short
+  "rolandmc303_perc:35",                //Whistle Short
+  "yamahary30_perc:10",                 //Whistle Short
+  "akaixr10_perc:14",                   //Whistle Mid
+  "rolands50_perc:13",                  //Whistle Mid
+  "yamahatg33_perc:9",                  //Whistle Mid
+  "yamahatg33_perc:10",                 //Whistle Mid
+  "rolandtr727_perc:9",                 //Whistle Long
+  "rolandr8_perc:6",                    //Whistle Long
+  "rolandmt32_perc:12",                 //Whistle Long
+  "rolandmc303_perc:25",                //Whistle Long
+];
