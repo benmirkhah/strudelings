8d950d73eb17d14b60c8506cb5acddf3cee7bbbd
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2025-11-15 17:59:54 -0700

    Cog Rattle sounds added

---
+++ spin.js
@@ -0,0 +1,24 @@
+var spins = [ //Ratchet / Cog Rattle spin sounds
+  "hh:1",                               //0 skip
+  "rolands50_misc:3",                   //1
+  "compurhythm78_misc:3",               //2
+  "compurhythm78_misc:2",               //3
+  "compurhythm78_misc:1",               //4
+  "compurhythm78_misc:4",               //5
+  "akaixr10_perc:7",                    //6
+  "akaixr10_perc:8",                    //7
+  "akaixr10_perc:13",                   //8
+  "rolandcompurhythm78_misc:1",         //9
+  //----------------------------------------
+  "rolands50_perc:8",                   //10
+  "rolandmt32_perc:8",                  //11
+  "rolandjd990_perc:2",                 //12
+  "rolandmc303_perc:24",                //13
+  "rolandmc303_perc:38",                //14
+  "rolandmc303_perc:39",                //15
+  "yamahary30_perc:5",                  //16
+  "rolandtr727_perc:5",                 //17
+  "rolandcompurhythm78_misc:2",         //18
+  "rolandcompurhythm78_misc:3",         //19
+  "rolandcompurhythm78_misc:4",         //20
+];
