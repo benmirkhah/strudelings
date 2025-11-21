d3665641abfc04df4a245af9a6f0904262164947
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2025-11-20 21:50:18 -0700

    Switched from array to object structure

--- spin.mjs
+++ spin.mjs
@@ -1,27 +1,26 @@
-const spins = [ //Ratchet / Cog Rattle spin sounds
-  "hh:1",                               //0 skip
-  "rolands50_misc:3",                   //1
-  "compurhythm78_misc:3",               //2
-  "compurhythm78_misc:2",               //3
-  "compurhythm78_misc:1",               //4
-  "compurhythm78_misc:4",               //5
-  "akaixr10_perc:7",                    //6
-  "akaixr10_perc:8",                    //7
-  "akaixr10_perc:13",                   //8
-  "rolandcompurhythm78_misc:1",         //9
-  //----------------------------------------
-  "rolands50_perc:8",                   //10
-  "rolandmt32_perc:8",                  //11
-  "rolandjd990_perc:2",                 //12
-  "rolandmc303_perc:24",                //13
-  "rolandmc303_perc:38",                //14
-  "rolandmc303_perc:39",                //15
-  "yamahary30_perc:5",                  //16
-  "rolandtr727_perc:5",                 //17
-  "rolandcompurhythm78_misc:2",         //18
-  "rolandcompurhythm78_misc:3",         //19
-  "rolandcompurhythm78_misc:4",         //20
-  "emusp12_misc:2",                     //21
-];
+/* Ratchet / Cog Rattle spin sounds */
+const spins = {
+  1: "rolands50_misc:3",
+  2: "compurhythm78_misc:3",
+  3: "compurhythm78_misc:2",
+  4: "compurhythm78_misc:1",
+  5: "compurhythm78_misc:4",
+  6: "akaixr10_perc:7",
+  7: "akaixr10_perc:8",
+  8: "akaixr10_perc:13",
+  9: "rolandcompurhythm78_misc:1",
+  10:"emusp12_misc:2",
+  11:"rolands50_perc:8",
+  12:"rolandmt32_perc:8",
+  13:"rolandjd990_perc:2",
+  14:"rolandmc303_perc:24",
+  15:"rolandmc303_perc:38",
+  16:"rolandmc303_perc:39",
+  17:"yamahary30_perc:5",
+  18:"rolandtr727_perc:5",
+  19:"rolandcompurhythm78_misc:2",
+  20:"rolandcompurhythm78_misc:3",
+  21:"rolandcompurhythm78_misc:4",
+};
 
 export default { spins }
